8a8293b1776cb0f42dedce4f7a9ddd75e37aaaaf
Dardo Altobelli <altobellidardo@gmail.com>
2024-08-17 16:20:40 -0300

    refac: redirect after sign in

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import db from "@astrojs/db";
 // https://astro.build/config
 export default defineConfig({
   integrations: [clerk({
-    signInFallbackRedirectUrl: '/'
+    signInFallbackRedirectUrl: '/new-user'
   }), db()],
   adapter: node({
     mode: "standalone"
