d78c4f5e3a253024c86529eaa7e72b91f8e5b28a
Dardo Altobelli <altobellidardo@gmail.com>
2024-08-17 16:19:50 -0300

    feat: add astro studio

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,75 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column, sql } from 'astro:db';
+
+const workshops = defineTable({
+  columns: {
+    id: column.text({
+      primaryKey: true,
+      unique: true,
+      default: sql`gen_random_uuid()`,
+      optional: false
+    }),
+    name: column.text({
+      unique: true
+    }),
+    description: column.text({
+      optional: true
+    }),
+    image_url: column.text({
+      optional: true
+    }),
+    capacity: column.number(),
+    teachers: column.text()
+  }
+})
+
+const roles = defineTable({
+  columns: {
+    name: column.text({
+      unique: true,
+      primaryKey: true
+    })
+  }
+})
+
+const users = defineTable({
+  columns: {
+    id: column.text({
+      primaryKey: true,
+      unique: true,
+      default: sql`gen_random_uuid()`
+    }),
+    name: column.text(),
+    email: column.text({
+      unique: true
+    }),
+    role: column.text({
+      references: () => roles.columns.name
+    })
+  }
+})
+
+const registrations = defineTable({
+  columns: {
+    id: column.text({
+      primaryKey: true,
+      unique: true,
+      default: sql`gen_random_uuid()`
+    }),
+    workshop_id: column.text({
+      references: () => workshops.columns.id
+    }),
+    user_id: column.text({
+      references: () => users.columns.id
+    })
+  }
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    workshops,
+    roles,
+    users,
+    registrations
+  }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,33 @@
-import { db } from 'astro:db';
+import { db, workshops, roles, users } from 'astro:db';
+
+const initialWorkshops = [
+	{
+		name: 'Workshop 1',
+		description: 'Workshop 1 description',
+		image_url: 'https://via.placeholder.com/150',
+		capacity: 10,
+		teachers: 'Teacher 1, Teacher 2'
+	}
+]
+
+const initalRoles = [
+	{ name: 'admin' },
+	{ name: 'user' }
+]
+
+const initialUsers = [
+	{
+		name: 'Dardo Altobelli',
+		email: 'daltobelli@etrr.edu.ar',
+		role: 'admin'
+	}
+]
+
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	await db.insert(workshops).values(initialWorkshops)
+	await db.insert(roles).values(initalRoles)
+	await db.insert(users).values(initialUsers)
+	console.log('Database seeded!')
 }
