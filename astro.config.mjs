451d63e1820095b1b9eb0f441216afbc7ca9bd51
Dardo Altobelli <altobellidardo@gmail.com>
2024-08-17 10:02:13 -0300

    chore: add astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import clerk from "@clerk/astro";
+import db from "@astrojs/db";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [clerk()],
-  adapter: node({ mode: "standalone" }),
-  output: "server",
+  integrations: [clerk({
+    signInFallbackRedirectUrl: '/'
+  }), db()],
+  adapter: node({
+    mode: "standalone"
+  }),
+  output: "server"
 });
