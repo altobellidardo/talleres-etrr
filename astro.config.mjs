620e342a2603df67fdeef2aa2cf74738d661acfc
Dardo Altobelli <altobellidardo@gmail.com>
2024-08-31 18:18:08 -0300

    chore: add react + tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import clerk from "@clerk/astro";
 import db from "@astrojs/db";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [clerk({
     signInFallbackRedirectUrl: '/new-user'
-  }), db()],
+  }), db(), react(), tailwind()],
   adapter: node({
     mode: "standalone"
   }),
